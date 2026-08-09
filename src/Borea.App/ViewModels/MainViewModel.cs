945381d7327af51a05d38e7e2bc1c45834e4440c
PlazmaBoltz <gjcerruti@outlook.com>
2026-08-08 20:55:29 -0400

    started designing based on mockup designs (not done)

--- src/Borea.App/ViewModels/MainViewModel.cs
+++ src/Borea.App/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Borea.App.ViewModels;
 
@@ -6,4 +7,37 @@ public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
     public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+    [ObservableProperty]
+    private string _mainColor = "#248cc0";
+    [ObservableProperty]
+    private string _secondaryColor = "#26556c";
+    [ObservableProperty]
+    private string _globalPanelsColor = "#2029d2";
+    [ObservableProperty]
+    private string _currentWindowDiscover = "True";
+    [ObservableProperty]
+    private string _currentWindowLibrary = "False";
+    [ObservableProperty]
+    private string _currentWindowSettings = "False";
+    [RelayCommand]
+    public void SetMainWindowDiscover() // Will be used to set whatever is on the main window (discover, library, etc.)
+    {
+        CurrentWindowDiscover = "True";
+        CurrentWindowLibrary = "False";
+        CurrentWindowSettings = "False";
+    }
+    [RelayCommand]
+    public void SetMainWindowLibrary() // Will be used to set whatever is on the main window (discover, library, etc.)
+    {
+        CurrentWindowDiscover = "False";
+        CurrentWindowLibrary = "True";
+        CurrentWindowSettings = "False";
+    }
+    [RelayCommand]
+    public void SetMainWindowSettings() // Will be used to set whatever is on the main window (discover, library, etc.)
+    {
+        CurrentWindowDiscover = "False";
+        CurrentWindowLibrary = "False";
+        CurrentWindowSettings = "True";
+    }
 }
