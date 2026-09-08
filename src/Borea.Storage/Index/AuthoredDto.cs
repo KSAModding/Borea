9cdef5a5aa9514fbc9a4f6125eb065a5a2be8b78
Jeremy Harrington <harringtonj6@xavier.edu>
2026-09-07 23:16:47 -0400

    Added JSON Data Transfer Objects to use a JSON serializer for easy validation and data transfer.

---
+++ src/Borea.Storage/Index/AuthoredDto.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Borea.Storage.Index;
+
+public sealed class AuthoredDto
+{
+    [JsonPropertyName("spec_version")]
+    public required int SpecVersion { get; set; }
+
+    [JsonPropertyName("id")]
+    public required string Id { get; set; }
+
+    [JsonPropertyName("type")]
+    public required string Type { get; set; }
+
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+
+    [JsonPropertyName("authors")]
+    public required List<string> Authors { get; set; }
+
+    [JsonPropertyName("abstract")]
+    public required string Abstract { get; set; }
+
+    [JsonPropertyName("description")]
+    public string? Description { get; set; }
+
+    [JsonPropertyName("license")]
+    public required string License { get; set; }
+
+    [JsonPropertyName("tags")]
+    public List<string>? Tags { get; set; }
+
+    [JsonPropertyName("status")]
+    public string? Status { get; set; }
+
+    [JsonPropertyName("superseded_by")]
+    public string? SupersededBy { get; set; }
+
+    [JsonPropertyName("compatibility")]
+    public required CompatibilityDto Compatibility { get; set; }
+
+    [JsonPropertyName("links")]
+    public required LinksDto Links { get; set; }
+
+    // Fails silently as a broken [releases] shouldn't break the whole Authored section
+    [JsonPropertyName("releases")]
+    public JsonElement? Releases { get; set; }
+
+    // Fails loudly if a loader is declared but can't be parsed
+    [JsonPropertyName("loader")]
+    public LoaderDto? Loader { get; set; }
+
+    // Fails loudly if dependencies are declared but can't be parsed
+    [JsonPropertyName("dependencies")]
+    public List<DependencyEntryDto>? Dependencies { get; set; }
+}
+
---
+++ src/Borea.Storage/Index/LoaderDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Borea.Storage.Index;
+
+public sealed class LoaderDto
+{
+    [JsonPropertyName("id")]
+    public required int Id { get; set; }
+
+    [JsonPropertyName("min")]
+    public required string Min { get; set; }
+
+    [JsonPropertyName("max")]
+    public string? Max { get; set; }
+
+    [JsonPropertyName("source")]
+    public string? Source { get; set; }
+}
