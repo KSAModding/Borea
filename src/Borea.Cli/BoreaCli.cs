6ba048ae2aba3c53974f4b23e7d882dd78721943
Maximilian Neßlauer <dev@max-nesslauer.com>
2026-09-13 01:52:33 +0200

    Add CLI install remove and update commands

--- src/Borea.Cli/BoreaCli.cs
+++ src/Borea.Cli/BoreaCli.cs
@@ -24,6 +24,9 @@ internal static class BoreaCli
         root.Subcommands.Add(IndexCommand.Build(services));
         root.Subcommands.Add(SearchCommand.Build(services));
         root.Subcommands.Add(ShowCommand.Build(services));
+        root.Subcommands.Add(ModInstallCommands.BuildInstall(services));
+        root.Subcommands.Add(ModInstallCommands.BuildRemove(services));
+        root.Subcommands.Add(ModInstallCommands.BuildUpdate(services));
         root.Subcommands.Add(InstanceCommand.Build(services));
         root.Subcommands.Add(ModStateCommands.BuildEnable(services));
         root.Subcommands.Add(ModStateCommands.BuildDisable(services));
